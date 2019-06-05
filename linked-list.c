9df0ea513ca2b7f5708555d2d87825e0cb1e7abf
caelan <caelanritter@gmail.com>
2019-06-05 12:05:14 -0400

    Add files.

---
+++ linked-list.c
@@ -0,0 +1,153 @@
+
+#include <string.h>
+
+#include "linked-list.h"
+
+/* create a single node with the given data at the given memory
+   location */
+int node_create(node_t **head_p, void *data) {
+  *head_p = malloc(sizeof(node_t));
+  if (*head_p == NULL) { return -1; }
+  (*head_p)->data = data;
+  (*head_p)->next = NULL;
+  return 0;
+}
+
+/* jumps to the next node in the linked list, and returns -1 if the
+   current node is NULL */
+int node_jump(node_t **current_p) {
+  if (*current_p == NULL) { return -1; }
+  *current_p = (*current_p)->next;
+  return 0;
+}
+
+/* jumps to the dth node relative to the current node, and returns -1
+   if the leap went off the end of the linked list */
+int node_leap(node_t **current_p, size_t d) {
+  size_t i;
+  int ret_code = 0;
+  for (i = 0; i < d; i++) {
+    ret_code += node_jump(current_p);
+  }
+  if (ret_code < -1) { ret_code = -1; }
+  return ret_code;
+}
+
+/* prepends a node with the given data to the linked list and jumps
+   the head node back to this new node */
+int node_prepend(node_t **head0_p, void *data) {
+  node_t *head1;
+  if (node_create(&head1, data) == -1) { return -1; }
+  head1->next = *head0_p;
+  *head0_p = head1;
+  return 0;
+}
+
+/* appends a node with the given data to the end of the linked list
+   without changing the current node */
+int node_append(node_t *current0, void *data) {
+  node_t *tail, *current1 = current0;
+  if (node_create(&tail, data) == -1) { return -1; }
+  while (current1->next != NULL) {
+    node_jump(&current1);
+  }
+  current1->next = tail;
+  return 0;
+}
+
+int node_insert(node_t *current, void *data) {
+  node_t *next0, *next1 = current->next;
+  if (node_create(&next0, data) == -1) { return -1; }
+  next0->next = next1;
+  current->next = next0;
+  return 0;
+}
+int node_adjoin(node_t *tail0, node_t *head1, bool force_shred) {
+  if (tail0->next == NULL) {
+    tail0->next = head1;
+    return 0;
+  }
+  else if (force_shred) {
+    node_shred(tail0->next);
+    tail0->next = head1;
+    return 1;
+  }
+  else { return -1; }
+}
+
+int node_remove(node_t *previous) {
+  if (previous->next == NULL) { return -1; }
+  node_t *next1 = previous->next->next;
+  free(previous->next->data);
+  free(previous->next);
+  previous->next = next1;
+  return 0;
+}
+
+int node_shred(node_t *current) {
+  if (current == NULL) { return -1; }
+  node_t *next = current->next;
+  current->next = NULL;
+  current = next;
+  while (current != NULL) {
+    next = current->next;
+    free(current->data);
+    free(current);
+    current = next;
+  }
+  return 0;
+}
+
+/*  removes the head node and switches the head node pointer to the
+    next node in the list */
+int node_presnip(node_t **head0_p) {
+  node_t *head1 = (*head0_p)->next;
+  free((*head0_p)->data);
+  free(*head0_p);
+  *head0_p = head1;
+  return 0;
+}
+
+int node_erase(node_t *current) {
+  free(current->data);
+  return 0;
+}
+
+int node_set(node_t *current, void *data, bool force_erase) {
+  if (current->data != NULL) {
+    if (!force_erase) { return -1; }
+    node_erase(current);
+  }
+  current->data = data;
+  return 0;
+}
+
+void * node_get(node_t *current) {
+  if (current == NULL) { return NULL; }
+  return current->data;
+}
+
+int node_copy(node_t *source, node_t **dest_p,
+	      size_t data_size, bool force_shred) {
+  if (*dest_p != NULL) {
+    if (!force_shred) { return -1; }
+    node_shred(*dest_p);
+    free((*dest_p)->data);
+    free(*dest_p);
+  }
+  node_t *current0 = source;
+  void *data1;
+  if ((data1 = malloc(data_size)) == NULL) { return -1; }
+  memcpy(data1, current0->data, data_size);
+  if (node_create(dest_p, data1) == -1) { return -1; }
+  node_t *current1 = *dest_p;
+  node_jump(&current0);
+  while (current0 != NULL) {
+    if ((data1 = malloc(data_size)) == NULL) { return -1; }
+    memcpy(data1, current0->data, data_size);
+    if (node_append(current1, data1) == -1) { return -1; }
+    node_jump(&current0);
+    node_jump(&current1);
+  }
+  return 0;
+}
---
+++ main.c
@@ -0,0 +1,361 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "linked-list.h"
+
+#ifndef max
+	#define max( a, b ) ( ((a) > (b)) ? (a) : (b) )
+#endif
+
+#ifndef min
+	#define min( a, b ) ( ((a) < (b)) ? (a) : (b) )
+#endif
+
+#define g 13u //genus
+#define r 5u //rank
+#define k 5u //torsion
+
+size_t global_max;
+
+typedef struct cell cell_t;
+struct cell {
+  size_t x;
+  size_t y;
+  size_t val;
+};
+
+/* "flocs" means "first locations"; it is the object passed to and
+   from an iterative step in the decisional tree; it records the first
+   (x,y)-position of each symbol under g and the max symbol that
+   appears */
+typedef struct flocs flocs_t;
+struct flocs {
+  size_t x[g];
+  size_t y[g];
+  size_t max;
+};
+
+flocs_t * iterate(node_t *, flocs_t *);
+flocs_t * cycle_values(cell_t *, node_t *, flocs_t *, flocs_t *);
+int is_subantidiagonal(node_t *);
+int flocs_copy(flocs_t *, flocs_t **);
+cell_t * cell_init(size_t, size_t, size_t);
+int cell_copy(cell_t *, cell_t **);
+void print_boundary(node_t *);
+void print_flocs(flocs_t *);
+
+int main(void) {
+  /* these initial assignments are made without loss of generality */
+  node_t *head = NULL;
+  node_create(&head, cell_init(2, 1, 2));
+  node_append(head, cell_init(1, 1, 1));
+  flocs_t *flocs = malloc(sizeof(flocs_t));
+  flocs->x[1] = 1;
+  flocs->y[1] = 1;
+  flocs->x[2] = 2;
+  flocs->y[2] = 1;
+  flocs->max = 2;
+  global_max = g;
+  
+  clock_t t = clock();
+  flocs_t *flocs_opt = iterate(head, flocs);
+  t = clock() - t;
+  
+  print_flocs(flocs_opt);
+  printf("runtime: %f\n----------------------\n", 
+	 (double)t/(double)CLOCKS_PER_SEC);
+  
+  return EXIT_SUCCESS;
+}
+
+/* -------------function definitions------------- */
+
+/* takes as input the head node of a boundary and a corresponding
+   flocs_t object and returns a flocs object with minimizes the max
+   (number of symbols used) */
+flocs_t * iterate(node_t *bdry_hd0, flocs_t *flocs0) {
+  //printf("CALLED\n");
+  //print_boundary(bdry_hd0);
+  //print_flocs(flocs0);
+  
+  node_t *bdry_cur0 = NULL, *bdry_nxt0 = NULL, *bdry_hd1 = NULL,
+    *bdry_prv1 = NULL, *bdry_cur1 = NULL, *bdry_nxt1 = NULL, *bdry_xxt1 = NULL;
+  cell_t *cell_cur0 = NULL, *cell_nxt0 = NULL, *c = NULL;
+  flocs_t *flocs1 = NULL, *flocs_tmp = NULL, *flocs_opt = NULL;
+  int dist = 0; //dist measures the distance along the boundary
+  
+  bdry_cur0 = bdry_hd0;
+  bdry_nxt0 = bdry_hd0;
+  node_jump(&bdry_nxt0);
+  
+  cell_cur0 = (cell_t *) node_get(bdry_cur0);
+  cell_nxt0 = (cell_t *) node_get(bdry_nxt0);
+  
+  flocs_opt = malloc(sizeof(flocs_t));
+  flocs_opt->max = global_max;
+  flocs_copy(flocs0, &flocs1);
+  flocs1->max++;
+  
+  /* the first cell */
+  cell_copy(cell_cur0, &c);
+  c->x++;
+  c->val++;
+  if (c->x < r + 1 && c->val < flocs_opt->max) {
+    node_copy(bdry_hd0, &bdry_hd1, sizeof(cell_t), true);
+    node_prepend(&bdry_hd1, c);
+    if (cell_nxt0->x == cell_cur0->x) {
+      node_remove(bdry_hd1);
+    }
+    flocs_tmp = cycle_values(c, bdry_hd1, flocs0, flocs1);
+    if (flocs_tmp->max < flocs_opt->max) {
+      free(flocs_opt);
+      flocs_opt = flocs_tmp;
+      global_max = flocs_opt->max;
+    }
+  }
+
+  /* still the first cell */
+  if (cell_nxt0->x < cell_cur0->x &&
+      cell_nxt0->y > cell_cur0->y) {
+    cell_copy(cell_cur0, &c);
+    c->y++;
+    c->val = max(cell_nxt0->val, c->val) + 1;
+    if (c->x + c->y < r + 2 && c->val < flocs_opt->max) {
+
+      node_copy(bdry_hd0, &bdry_hd1, sizeof(cell_t), true);
+      bdry_xxt1 = bdry_hd1;
+      bdry_cur1 = bdry_xxt1;
+      node_jump(&bdry_xxt1);
+      bdry_nxt1 = bdry_xxt1;
+      node_jump(&bdry_xxt1);
+      node_insert(bdry_cur1, c);
+
+      if (bdry_xxt1 != NULL &&
+	  ((cell_t *)node_get(bdry_xxt1))->x ==
+	  ((cell_t *)node_get(bdry_nxt1))->x) {
+	node_remove(bdry_cur1->next);
+      }
+
+      flocs_tmp = cycle_values(c, bdry_hd1, flocs0, flocs1);
+      if (flocs_tmp->max < flocs_opt->max) {
+	free(flocs_opt);
+	flocs_opt = flocs_tmp;
+	global_max = flocs_opt->max;
+      }
+    }
+  }
+
+  node_jump(&bdry_cur0);
+  node_jump(&bdry_nxt0);
+  cell_cur0 = node_get(bdry_cur0);
+  cell_nxt0 = node_get(bdry_nxt0);
+  dist++;
+
+  if (cell_nxt0 != NULL) {
+    /* the middle cells */
+    while(bdry_nxt0->next != NULL) {
+      if (cell_nxt0->x < cell_cur0->x &&
+	  cell_nxt0->y > cell_cur0->y) {
+	cell_copy(cell_cur0, &c);
+	c->y++;
+	c->val = max(cell_nxt0->val, c->val) + 1;
+	if (c->x + c->y < r + 2 && c->val < flocs_opt->max) {
+
+	  node_copy(bdry_hd0, &bdry_hd1, sizeof(cell_t), true);
+	  bdry_xxt1 = bdry_hd1;
+	  node_leap(&bdry_xxt1, dist-1);
+	  bdry_prv1 = bdry_xxt1;
+	  node_jump(&bdry_xxt1);
+	  bdry_cur1 = bdry_xxt1;
+	  node_jump(&bdry_xxt1);
+	  bdry_nxt1 = bdry_xxt1;
+	  node_jump(&bdry_xxt1);
+	  node_insert(bdry_cur1, c);
+
+	  if (((cell_t *)node_get(bdry_xxt1))->x ==
+	      ((cell_t *)node_get(bdry_nxt1))->x) {
+	    node_remove(bdry_cur1->next);
+	  }
+	  if (((cell_t *)node_get(bdry_prv1))->y ==
+	      ((cell_t *)node_get(bdry_cur1))->y) {
+	    node_remove(bdry_prv1);
+	  }
+
+	  flocs_tmp = cycle_values(c, bdry_hd1, flocs0, flocs1);
+	  if (flocs_tmp->max < flocs_opt->max) {
+	    free(flocs_opt);
+	    flocs_opt = flocs_tmp;
+	    global_max = flocs_opt->max;
+	  }
+	}
+      }
+      node_jump(&bdry_cur0);
+      node_jump(&bdry_nxt0);
+      cell_cur0 = node_get(bdry_cur0);
+      cell_nxt0 = node_get(bdry_nxt0);
+      dist++;
+    }
+  
+    /* the penultimate cell */
+    if (cell_nxt0->x < cell_cur0->x &&
+	cell_nxt0->y > cell_cur0->y) {
+      cell_copy(cell_cur0, &c);
+      c->y++;
+      c->val = max(cell_nxt0->val, c->val) + 1;
+      if (c->x + c->y < r + 2 && c->val < flocs_opt->max) {
+      
+	node_copy(bdry_hd0, &bdry_hd1, sizeof(cell_t), true);
+	bdry_nxt1 = bdry_hd1;
+	node_leap(&bdry_nxt1, dist - 1);
+	bdry_prv1 = bdry_nxt1;
+	node_jump(&bdry_nxt1);
+	bdry_cur1 = bdry_nxt1;
+	node_jump(&bdry_nxt1);
+	node_insert(bdry_cur1, c);
+
+	if (((cell_t *)node_get(bdry_prv1))->y ==
+	    ((cell_t *)node_get(bdry_cur1))->y) {
+	  node_remove(bdry_prv1);
+	}
+
+	flocs_tmp = cycle_values(c, bdry_hd1, flocs0, flocs1);
+	if (flocs_tmp->max < flocs_opt->max) {
+	  free(flocs_opt);
+	  flocs_opt = flocs_tmp;
+	  global_max = flocs_opt->max;
+	}
+      }
+    }
+    
+    node_jump(&bdry_cur0);
+    cell_cur0 = node_get(bdry_cur0);
+    dist++;
+  }
+  
+  /* the last cell */
+  cell_copy(cell_cur0, &c);
+  c->y++;
+  c->val++;
+  if (c->y < r + 1 && c->val < flocs_opt->max) {
+    node_copy(bdry_hd0, &bdry_hd1, sizeof(cell_t), true);
+    bdry_cur1 = bdry_hd1;
+    node_leap(&bdry_cur1, dist - 1);
+    bdry_prv1 = bdry_cur1;
+    node_jump(&bdry_cur1);
+    node_insert(bdry_cur1, c);
+    
+    if (((cell_t *)node_get(bdry_prv1))->y ==
+	((cell_t *)node_get(bdry_cur1))->y) {
+      node_remove(bdry_prv1);
+    }
+    
+    flocs_tmp = cycle_values(c, bdry_hd1, flocs0, flocs1);
+    if (flocs_tmp->max < flocs_opt->max) {
+      free(flocs_opt);
+      flocs_opt = flocs_tmp;
+      global_max = flocs_opt->max;
+    }
+  }
+  //printf("RETURNING\n");
+  //print_flocs(flocs0);
+  //print_flocs(flocs_opt);
+  /* if flocs_opt was not changed, all the above cases failed (and
+     cycle_values() was never called): this occured either because the
+     boundary coincides with the sub-antidiagonal, in which case we
+     return flocs0, or because global_max is low enough that we can't
+     add any more cells, in which case we return flocs_opt; if neither
+     is true, we return flocs_opt */
+  if (flocs_opt->x[1] == 0 && is_subantidiagonal(bdry_hd0)) {
+    //printf("RETURNED FLOCS0\n----------------------\n");
+    return flocs0;
+  }
+  else {
+    //printf("RETURNED FLOCS_OPT\n----------------------\n");
+    return flocs_opt;
+  }
+}
+
+/* for a given cell, it runs iterate() with the augmented boundary and
+   flocs on each valid symbol */
+flocs_t * cycle_values(cell_t *c, node_t *bdry_hd1,
+		       flocs_t *flocs0, flocs_t *flocs1) {
+  flocs_t *flocs_tmp = NULL, *flocs_opt = NULL;
+  flocs_opt = malloc(sizeof(flocs_t));
+  flocs_opt->max = global_max;
+  for ( ; c->val <= flocs0->max; c->val++) {
+    if ((c->x - c->y - flocs0->x[c->val] + flocs0->y[c->val]) % k == 0) {
+      flocs_tmp = iterate(bdry_hd1, flocs0);
+      if (flocs_tmp->max < flocs_opt->max) {
+	free(flocs_opt);
+	flocs_opt = flocs_tmp;
+	global_max = flocs_opt->max;
+      }
+    }
+  }
+  if (c->val < global_max) {
+    flocs1->x[c->val] = c->x;
+    flocs1->y[c->val] = c->y;
+    flocs_tmp = iterate(bdry_hd1, flocs1);
+    if (flocs_tmp->max < flocs_opt->max) {
+      free(flocs_opt);
+      flocs_opt = flocs_tmp;
+      global_max = flocs_opt->max;
+    }
+  }
+  return flocs_opt;
+}
+
+int is_subantidiagonal(node_t * current) {
+  while(current != NULL) {
+    cell_t *c = (cell_t *)node_get(current);
+    if (c->x + c->y < r + 1) { return 0; }
+    node_jump(&current);
+  }
+  return 1;
+}
+
+cell_t * cell_init(size_t x, size_t y, size_t val) {
+  cell_t *c = malloc(sizeof(cell_t));
+  c->x = x;
+  c->y = y;
+  c->val = val;
+  return c;
+}
+
+int cell_copy(cell_t *src, cell_t **dest) {
+  if (((*dest) = malloc(sizeof(cell_t))) == NULL) { return -1; }
+  (*dest)->x = src->x;
+  (*dest)->y = src->y;
+  (*dest)->val = src->val;
+  return 0;
+}
+
+int flocs_copy(flocs_t *src, flocs_t **dest) {
+  if (((*dest) = malloc(sizeof(flocs_t))) == NULL) { return -1; }
+  size_t i;
+  for (i = 1; i <= src->max; i++) {
+    (*dest)->x[i] = src->x[i];
+    (*dest)->y[i] = src->y[i];
+  }
+  (*dest)->max = src->max;
+  return 0;
+}
+
+void print_boundary(node_t *current) {
+  cell_t *c;
+  while (current != NULL) {
+    c = (cell_t *) node_get(current);
+    printf("x=%ld\ty=%ld\tval=%ld\n", c->x, c->y, c->val);
+    node_jump(&current);
+  }
+  printf("----------------------\n");
+}
+void print_flocs(flocs_t *flocs) {
+  size_t i;
+  printf("max: %ld\n", flocs->max);
+  for (i = 1; i <= flocs->max; i++) {
+    printf("%ld at (%ld, %ld)\n", i, flocs->x[i], flocs->y[i]);
+  }
+  printf("----------------------\n");
+}
